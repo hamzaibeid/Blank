c35de406840c6ae08ef7b5452364125b05de8f23
Hamza Ibeid <hamzaibeid@hotmail.com>
2022-01-03 20:23:56 +0000

    fixing dead ends

--- Assets/_Scripts/CorridoreFirstDungeonGenerator.cs
+++ Assets/_Scripts/CorridoreFirstDungeonGenerator.cs
@@ -11,26 +11,64 @@ public class CorridoreFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
     [Range(0.1f, 1)]
     private float roomPercent = 0.8f;
     
-  
-
-
-
     protected override void RunProcidularGenerator()
     {
-        CorridoreFirstDungeonGeneration();
+        CorridoreFirstGeneration();
     }
 
-    private void CorridoreFirstDungeonGeneration()
+    private void CorridoreFirstGeneration()
     {
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPosintions = new HashSet<Vector2Int>();
         CreateCoridores(floorPositions,potentialRoomPosintions);
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPosintions);
         //create rooms and corridores
+        List<Vector2Int> deadEnds = FindAllDeadEnds(floorPositions);
+        CreateRoomsAtDeadEnd(deadEnds,roomPositions);
         floorPositions.UnionWith(roomPositions);
         tileMapVisualizer.PaintFloorTiles(floorPositions);
         WallGenerator.CreateWalls(floorPositions, tileMapVisualizer);
     }
+
+    private void CreateRoomsAtDeadEnd(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors)
+    {
+        //Creating rooms at dead ends
+        foreach (var position in deadEnds)
+        {
+            if(roomFloors.Contains(position) == false)
+            {
+                var room = RunRandomWalk(randomWalkParameters,position);
+                roomFloors.UnionWith(room);
+            }
+        }
+    }
+
+    private List<Vector2Int> FindAllDeadEnds(HashSet<Vector2Int> floorPositions)
+    {
+
+        List<Vector2Int> deadEnds = new List<Vector2Int>();
+        //finding dead ends
+        foreach (var position in floorPositions)
+        {
+            int neighborsCount = 0;
+            foreach (var direction in Direction2D.cardinalDirectionsList)
+            {
+                if (floorPositions.Contains(position + direction))
+                {
+                    neighborsCount++;
+                    
+                }
+
+            }
+            if(neighborsCount == 1)
+            {
+                deadEnds.Add(position);
+            }
+
+        }
+        return deadEnds;
+    }
+
 
     private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPosintions)
     {
