353063b019d049554b054c3a5c5c1c6460a44a38
Hamza Ibeid <hamzaibeid@hotmail.com>
2022-01-03 19:59:01 +0000

    Combinig rooms with corridors

--- Assets/_Scripts/CorridoreFirstDungeonGenerator.cs
+++ Assets/_Scripts/CorridoreFirstDungeonGenerator.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CorridoreFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
 {
     [SerializeField]
-    private int corridoreLength = 14,coridoreCount = 5;
+    private int corridoreLength = 14, coridoreCount = 5;
     [SerializeField]
-    [Range(0.1f,1)]
+    [Range(0.1f, 1)]
     private float roomPercent = 0.8f;
-    [SerializeField]
-    public SimpleRandomWalkSO roomGenerationParameters;
-
     
+  
+
+
 
     protected override void RunProcidularGenerator()
     {
@@ -23,19 +23,45 @@ public class CorridoreFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
     private void CorridoreFirstDungeonGeneration()
     {
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
-        CreateCoridores(floorPositions);
+        HashSet<Vector2Int> potentialRoomPosintions = new HashSet<Vector2Int>();
+        CreateCoridores(floorPositions,potentialRoomPosintions);
+        HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPosintions);
+        //create rooms and corridores
+        floorPositions.UnionWith(roomPositions);
         tileMapVisualizer.PaintFloorTiles(floorPositions);
-        WallGenerator.CreateWalls(floorPositions,tileMapVisualizer);
+        WallGenerator.CreateWalls(floorPositions, tileMapVisualizer);
     }
 
-    private void CreateCoridores(HashSet<Vector2Int> floorPositions)
+    private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPosintions)
+    {
+
+        HashSet<Vector2Int> roomPositions = new HashSet<Vector2Int>();
+        int roomToCreateCount = Mathf.RoundToInt(potentialRoomPosintions.Count*roomPercent);
+
+        //take rooms by random
+        List<Vector2Int> roomToCreate = potentialRoomPosintions.OrderBy(x => Guid.NewGuid()).Take(roomToCreateCount).ToList();
+        foreach (var roomPosition in roomToCreate)
+        {
+            //generate rooms
+            var roomFloor = RunRandomWalk(randomWalkParameters, roomPosition);
+            //avoid repetition
+            roomPositions.UnionWith(roomFloor);
+        }
+        return roomPositions;
+
+    }
+
+    private void CreateCoridores(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPosintions)
     {
         var currentPosition = startPos;
+        potentialRoomPosintions.Add(currentPosition);
         for (int i = 0; i < coridoreCount; i++)
         {
-            var corridore = ProceduralGenerationAlgorithims.RandomWalkCorridore(currentPosition,corridoreLength);
+            var corridore = ProceduralGenerationAlgorithims.RandomWalkCorridore(currentPosition, corridoreLength);
             currentPosition = corridore[corridore.Count - 1];
+            potentialRoomPosintions.Add(currentPosition);
             floorPositions.UnionWith(corridore);
         }
     }
+
 }
--- Assets/_Scripts/SimpleRandomWalkDungeonGenerator.cs
+++ Assets/_Scripts/SimpleRandomWalkDungeonGenerator.cs
@@ -9,23 +9,25 @@ public class SimpleRandomWalkDungeonGenerator : AbstractDungeonGenerator
 {
 
     [SerializeField]
-    private SimpleRandomWalkSO randomWalkParameters;
+    protected SimpleRandomWalkSO randomWalkParameters;
+
 
 
 
 
     protected override void RunProcidularGenerator()
     {
-        HashSet<Vector2Int> floorPosition = RunRandomWalk(randomWalkParameters);
+        HashSet<Vector2Int> floorPosition = RunRandomWalk(randomWalkParameters,startPos);
         tileMapVisualizer.clear();
         tileMapVisualizer.PaintFloorTiles(floorPosition);
         WallGenerator.CreateWalls(floorPosition,tileMapVisualizer);
 
     }
 
-    protected HashSet<Vector2Int> RunRandomWalk(SimpleRandomWalkSO parameters)
+    protected HashSet<Vector2Int> RunRandomWalk(SimpleRandomWalkSO parameters,Vector2Int position)
     {
-        var currentPosition = startPos;
+
+        var currentPosition = position;
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         for (int i = 0; i < parameters.itterations; i++)
         {
