296aa1d1517624fa3400974f4b51a63b554381fe
Hamza Ibeid <hamzaibeid@hotmail.com>
2022-01-08 16:38:54 +0000

    Testing Player

---
+++ Assets/_Scripts/CameraMotor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta =  Vector3.zero;
+
+        // This check if we're inside the bounds of X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        // This check if we're inside the bounds of Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaX > boundY || deltaX < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+        
+
+        
+
+    }
+
+}
