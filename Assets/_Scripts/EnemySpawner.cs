04a3ccc48dd60ee9c52c748c825a532a66fbb05a
Hamza Ibeid <hamzaibeid@hotmail.com>
2022-01-09 22:39:04 +0000

    Enemies

---
+++ Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRaduis = 7, time = 1.5f;
+    public GameObject [] Enemies;
+
+    void Start()
+    {
+        StartCoroutine(SpawnAnEnemy());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnAnEnemy()
+    {
+        Vector2 spawnPosition = GameObject.Find("player_0").transform.position;
+        spawnPosition += Random.insideUnitCircle.normalized * spawnRaduis;
+        Instantiate(Enemies[Random.Range(0, Enemies.Length)], spawnPosition,Quaternion.identity);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(SpawnAnEnemy());
+        
+    }
+}
